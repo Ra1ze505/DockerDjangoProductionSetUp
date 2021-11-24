7df1b5cfe9dcce62db0621b3bd5076faea0ce469
Ra1ze505 <87398772+Ra1ze505@users.noreply.github.com>
2021-11-24 14:11:25 +0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -185,8 +185,8 @@ DJOSER = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
